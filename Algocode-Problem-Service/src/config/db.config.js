858378545c6253769ff96e4d8fe8ab297415652e
Kritikamishra-16 <mishrakritika2001@gmail.com>
2024-11-16 13:13:31 +0530

    Changes for making dbConnection as singleton class in

--- Algocode-Problem-Service/src/config/db.config.js
+++ Algocode-Problem-Service/src/config/db.config.js
@@ -1,15 +1,51 @@
 const mongoose = require('mongoose');
 const { NODE_ENV, ATLAS_DB_URL } = require('./server.config');
 
-async function connectToDB(){
-    try{
-        if(NODE_ENV== "development"){
-            await mongoose.connect(ATLAS_DB_URL);
+// async function connectToDB(){
+//     try{
+//         if(NODE_ENV== "development"){
+//             await mongoose.connect(ATLAS_DB_URL);
+//         }
+//     }catch(error){
+//         console.log('Unable to connect to the DB server');
+//         console.log(error);
+//     }
+// }
+
+// SINGLETON CLASS IMPLEMENTATION
+
+let instance; //stored db instance
+class DBConnection{
+    #isConnected; // private variable so that no one can access it from its object
+
+    constructor(db_uri){
+        if(instance){
+            // if the instance variable already has a value
+            throw new Error("Only one connection can exist");
         }
-    }catch(error){
-        console.log('Unable to connect to the DB server');
-        console.log(error);
+        this.uri = db_uri;
+        instance = this;  // "this" is the current object that will be created once the constructor has been called
+        this.#isConnected = false;
     }
+
+    async connect(){
+        if(this.#isConnected){
+            throw new Error("DB already connected");
+        }
+        if(NODE_ENV == "development"){
+            await mongoose.connect(ATLAS_DB_URL);
+            this.#isConnected = true;
+        }
+    }
+
+    async disconnect(){
+        this.#isConnected = false;
+    }
+
 }
 
-module.exports = connectToDB;
+//Freezing an object means preventing extensions and makes existing properties non-writeable and non-configurable
+const db = Object.freeze(new DBConnection());
+
+
+module.exports = db;
--- Algocode-Problem-Service/src/index.js
+++ Algocode-Problem-Service/src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.text());
 const { PORT } = require('./config/server.config');
 const BaseError = require('./errors/base.error');
 const errorHandler = require('./utils/errorHandler');
-const connectToDB = require('./config/db.config');
+const dbConnector = require('./config/db.config');
 
 //If any request comes and route starts with /api we map it to apiRoute
 app.use("/api",apiRouter);
@@ -25,7 +25,7 @@ app.use(errorHandler);
 
 app.listen(PORT, async ()=>{
     console.log(`Server started at PORT ${PORT}`);
-    await connectToDB();
+    await dbConnector.connect();
     console.log("Successfully connected to db");
 });
 
